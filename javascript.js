7160298f7de3d071ac46511d0cb55482844927cf
douglaslins <douglaslins00@gmail.com>
2015-12-08 23:21:59 -0200

    FINAL PULL

---
+++ javascript.js
@@ -0,0 +1,23 @@
+$(function(){
+
+$(".gomenu1").click(function() {
+$('html, body').animate({ scrollTop: 0 }, 600);
+});
+
+$(".gomenu2").click(function() {
+$('html, body').animate({ scrollTop: 1050 }, 600);
+});
+
+$(".gomenu3").click(function() {
+$('html, body').animate({ scrollTop: 2100 }, 600);
+});
+
+$(".gomenu4").click(function() {
+$('html, body').animate({ scrollTop: 2900 }, 600);
+});
+
+$(".gomenu5").click(function() {
+$('html, body').animate({ scrollTop: 3600 }, 600);
+});
+    
+});
